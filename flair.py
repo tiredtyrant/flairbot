c9e93d70b94f0dd62bf22510ed570735ef8ca4a8
tiredtyrant <tiredtyrant@gmail.com>
2014-10-10 18:10:28 -0300

    Verificacao de cidade e estado
    Signed-off-by: tiredtyrant <tiredtyrant@gmail.com>

--- flair.py
+++ flair.py
@@ -1,18 +1,53 @@
+# coding=utf-8
 import praw
+import sqlite3
+
+con = sqlite3.connect('estados_municipios.db')
+cursor = con.cursor()
+
+def dbLookup(msg):
+    if len(msg.split(',')) != 2:
+        return False
+    cidade = msg.split(',')[0].strip()
+    estado = msg.split(',')[1].strip()
+    #check estado
+    query = 'SELECT id FROM estados WHERE uf == \'%s\';' % (estado)
+    cursor.execute(query)
+    if not cursor.fetchone():
+        return False
+    #check cidade
+    query = 'SELECT municipios.id FROM municipios WHERE nome = \'%s\''% (cidade)
+    cursor.execute(query)
+    if not cursor.fetchone():
+        return False
+    #check cidade pertence ao estado
+    query = 'SELECT estados.id FROM municipios JOIN estados ON municipios.estados_id == estados.id WHERE municipios.nome == "%s" AND estados.uf == "%s";' % (cidade, estado)
+    cursor.execute(query)
+    if not cursor.fetchone():
+        return False
+        
+    return True
+    
 
 def main():
-    estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
     r = praw.Reddit(user_agent='flairbotbr')
     r.login('botbr', '***REMOVED***')
     while True:
         for msg in r.get_unread(limit=None):
-            print type(msg.subject)
-            print type(msg.author)
-            print type(msg.body)
-            #print "Subject: " + str(msg.subject)
-            #print "author: " + str(msg.author)
-            #print "body: " + str(msg.body)
-            flairstr = msg.subject
+            print msg.subject
+            print msg.author
+            print msg.body
+            sub = r.get_subreddit('brasil')
+            if msg.subject == 'flair' and dbLookup(msg.body):
+                flairstr = msg.body
+                estado = msg.body.split(',')[1].strip()
+                sub.set_flair(msg.author,flairstr,estado)
+            elif msg.subject == 'remover flair':
+                sub.set_flair(msg.author,'','')
+            else:
+                pass
+            msg.mark_as_read()
+            #r.send_message(msg.author,'flair','hello!')
             '''
             if len(flairstr.split(',')) == 2:
                 sigla_estado = flairstr.split(',')[1].strip()
@@ -20,7 +55,6 @@ def main():
                     sub = r.get_subreddit('brasil')
                     sub.set_flair(str(msg.author),flairstr,sigla_estado)
                     '''
-            #msg.mark_as_read()
                 
             
         '''
