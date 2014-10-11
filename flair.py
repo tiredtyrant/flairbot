43c992d5848ad958152b133f9334c48b92473d77
tiredtyrant <tiredtyrant@gmail.com>
2014-10-11 14:30:42 -0300

    Banco de dados atualizados com paises Envio de mensagem para o usuario indicando sucesso ou erro
    Signed-off-by: tiredtyrant <tiredtyrant@gmail.com>

--- flair.py
+++ flair.py
@@ -1,80 +1,64 @@
 # coding=utf-8
 import praw
 import sqlite3
+import time
 
 con = sqlite3.connect('estados_municipios.db')
 cursor = con.cursor()
 
 def dbLookup(msg):
     if len(msg.split(',')) != 2:
-        return False
-    cidade = msg.split(',')[0].strip()
-    estado = msg.split(',')[1].strip()
-    #check estado
-    query = 'SELECT id FROM estados WHERE uf == \'%s\';' % (estado)
-    cursor.execute(query)
-    if not cursor.fetchone():
-        return False
-    #check cidade
-    query = 'SELECT municipios.id FROM municipios WHERE nome = \'%s\''% (cidade)
-    cursor.execute(query)
-    if not cursor.fetchone():
-        return False
-    #check cidade pertence ao estado
-    query = 'SELECT estados.id FROM municipios JOIN estados ON municipios.estados_id == estados.id WHERE municipios.nome == "%s" AND estados.uf == "%s";' % (cidade, estado)
-    cursor.execute(query)
-    if not cursor.fetchone():
-        return False
-        
+        #procura na lista de paises
+        query = 'SELECT id FROM paises WHERE nome == "%s"' % (msg)
+        cursor.execute(query)
+        if cursor.fetchone():
+            return True
+        else:
+            return False
+    else:
+        cidade = msg.split(',')[0].strip()
+        estado = msg.split(',')[1].strip()
+        #check cidade pertence ao estado
+        query = 'SELECT estados.id FROM municipios JOIN estados ON municipios.estados_id == estados.id WHERE municipios.nome == "%s" AND estados.uf == "%s";' % (cidade, estado)
+        cursor.execute(query)
+        if not cursor.fetchone():
+            return False
+            
     return True
     
 
 def main():
     r = praw.Reddit(user_agent='flairbotbr')
-    r.login('botbr', '***REMOVED***')
+    r.login('botbr', 'apassword')
+    if r.is_logged_in():
+        print 'logged in'
+    else:
+        print 'failed to log in'
+        return
     while True:
+        time.sleep(0.5)
         for msg in r.get_unread(limit=None):
-            print msg.subject
-            print msg.author
-            print msg.body
+            try:
+                print 'AUTHOR: %s - SUBJECT: %s - BODY: %s' % (msg.author, msg.subject, msg.body)
+            except UnicodeEncodeError:
+                print 'AUTHOR: %s - unprintable chars' % (msg.author)
             sub = r.get_subreddit('brasil')
-            if msg.subject == 'flair' and dbLookup(msg.body):
-                flairstr = msg.body
-                estado = msg.body.split(',')[1].strip()
-                sub.set_flair(msg.author,flairstr,estado)
-            elif msg.subject == 'remover flair':
+            if msg.subject == 'flair':
+                if dbLookup(msg.body):
+                    estado = 'world' if len(msg.body.split(',')) < 2 else msg.body.split(',')[1].strip()
+                    sub.set_flair(msg.author,msg.body,estado)
+                    r.send_message(msg.author,'flair','Flair configurado.')
+                    print('flair ok')
+                else:
+                    r.send_message(msg.author,'flair','Configuração de flair falhou.')
+                    print('flair fail')
+            if msg.subject == 'remover flair':
                 sub.set_flair(msg.author,'','')
-            else:
-                pass
-            msg.mark_as_read()
-            #r.send_message(msg.author,'flair','hello!')
-            '''
-            if len(flairstr.split(',')) == 2:
-                sigla_estado = flairstr.split(',')[1].strip()
-                if sigla_estado in estados:
-                    sub = r.get_subreddit('brasil')
-                    sub.set_flair(str(msg.author),flairstr,sigla_estado)
-                    '''
+                r.send_message(msg.author,'flair','Flair removido.')
+                print('remove flair ok')
                 
-            
-        '''
-        if subj == 'crest':
-            print msg
-            auth = str(msg.author)
-            body = str(msg.body)
-            print "Author: " + auth
-            print "Message content: " + body
-            sub = r.get_subreddit('football')
-            if body in teams:
-                ftext = str(teams[body])
-                sub.set_flair(auth, ftext, body)
-                with open('log.txt', 'a') as logfile:
-                    tn = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-                    lm = ' : ' + body + ' @ ' + tn
-                    logfile.write('\n\rAdded: ' + auth + ' : ' + ftext + lm)
-                print "Setting flair: " + auth + " : " + ftext + " : " + body
-                msg.mark_as_read()
-                '''
+            msg.mark_as_read()
 
 if __name__ == '__main__':
     main()
+    
--- loadtosql.py
+++ loadtosql.py
@@ -3,6 +3,6 @@ import sqlite3 as sqlite
 con = sqlite.connect('estados_municipios.db')
 cur = con.cursor()
 
-script = open('estados_municipios.sql').read()
+script = open('estados_municipios.sql','r',encoding='utf-8').read()
 
 cur.executescript(script)
