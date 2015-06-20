00657b368470330cd7ea06b84f900b74527dca04
tiredtyrant <tiredtyrant@gmail.com>
2015-06-20 16:02:24 -0300

    Removidos script pra gerar banco e arquivo do banco
    Signed-off-by: tiredtyrant <tiredtyrant@gmail.com>

--- loadtosql.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 as sqlite
-
-con = sqlite.connect('estados_municipios.db')
-cur = con.cursor()
-
-script = open('estados_municipios.sql','r',encoding='utf-8').read()
-
-cur.executescript(script)
